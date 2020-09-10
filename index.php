9ca2b034a81b10a7d757f0694b06aaef228f377a
Hafiz Arslan Tariq <hafiz-arslan.tariq@audi.de>
2020-09-10 18:28:10 +0200

    without html form, the task is completed

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 error_reporting(E_ALL);
 require_once('user.php');
+require_once('t9search.php');
+
+$digits = "277";
+$combinations = get_combinations(str_split($digits));
 
 $u = new user();
-$data = $u->getNameOfUsers(['ass', "tar"]);
+$data = $u->getNameOfUsers($combinations);
 
 if ($data) {
     print_r($data);
