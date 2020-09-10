53bf9c58d892fef828014ff848b0de3641a36381
Hafiz Arslan Tariq <hafiz-arslan.tariq@audi.de>
2020-09-10 17:34:10 +0200

    update the query to fetch the first name and last name based on the t9 combinations

--- user.php
+++ user.php
@@ -4,8 +4,9 @@ require_once('db.php');
 
 class user extends db {
 
-    public function getNameOfUsers() {
-        $sql = "SELECT * from users";
+    public function getNameOfUsers($combinations) {
+        $regexp = implode("|", $combinations);
+        $sql = "SELECT first_name, last_name from users where CONCAT(first_name, ' ', last_name) REGEXP '{$regexp}'";
         $result = $this->conenct()->query($sql);
         $num_rows = $result->num_rows;
 
